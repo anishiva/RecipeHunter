14f793d66397aa207516f75fac2c8e72d0a14e80
Anitha Sivakumar <anishiva@ShivasMBP03.labs.subbu.us>
2016-05-03 22:38:43 -0400

    circle.yml

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,6 @@
 class Recipe
 	include HTTParty
-		key_value = ENV['FOOD2FORK_KEY'] || '0e20df322974e6e7b32b75e7dd7a8338'
+		key_value = ENV['FOOD2FORK_KEY'] #|| '0e20df322974e6e7b32b75e7dd7a8338'
 		hostport = ENV['FOOD2FORK_SERVER_AND_PORT'] || 'www.food2fork.com'
 		base_uri "http://#{hostport}/api"
 		default_params key: key_value
