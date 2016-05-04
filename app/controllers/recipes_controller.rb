ca8433e6c91d8baf79e2ca53f931031fd7e29608
Anitha Sivakumar <anishiva@ShivasMBP03.labs.subbu.us>
2016-05-04 18:43:59 -0400

    Pagination

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,6 @@
 class RecipesController < ApplicationController
   def index
   	@search_term = params[:search]
-  	@recipes = Recipe.for(@search_term)
+  	@recipes = Recipe.for(@search_term).paginate(page: params[:page], per_page: 5)
   end
 end
